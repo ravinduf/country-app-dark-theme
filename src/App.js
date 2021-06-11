490ec029f96b30960bb1a7ecdab5f29714fe7009
Ravindu Senal Fernando <ravinduf@rocketmail.com>
2021-06-11 11:17:06 +0530

    removed some unnecessary code

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
-
+import { useState } from 'react';
 
 function App() {
+  const [dark, setDark] = useState(true);
+
+  const handleDarkMode = () => {
+    setDark(prevState => !prevState)
+  }
   return (
-    <div className="App">
-      <h1>Hello</h1>
+    <div className={'theme ' + (dark ? 'theme--dark' : 'theme--default')}>
+      <div className="body">
+      <h1 className="heading">Hello</h1>
+      </div>
+      
     </div>
   );
 }
