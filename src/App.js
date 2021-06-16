e71d0d58376f7c5de1122273b2bfa4aa7f5e83df
Ravindu Senal Fernando <ravinduf@rocketmail.com>
2021-06-16 10:52:59 +0530

    fixed margin and layout issues

--- src/App.js
+++ src/App.js
@@ -3,6 +3,11 @@ import classNames from 'classnames';
 
 import Header from './components/Header/Header';
 import Search from './components/Search/Search';
+import Filter from './components/Filter/Filter';
+
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+import Container from 'react-bootstrap/Container'
 
 function App() {
 
@@ -13,7 +18,15 @@ function App() {
       {/* className={'theme ' + (dark ? 'theme--dark' : 'theme--default') */}
       <div className="app">
         <Header />
-        <Search />
+
+        <Row className="mt-5">
+          <Col md={6}>
+            <Search />  
+          </Col>
+          <Col>
+            <Filter/>
+          </Col>
+        </Row>
       </div>
       
     </div>
