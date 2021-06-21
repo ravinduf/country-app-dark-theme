55c2cc97098ed864f5ffd3515748e10a1ee06e75
Ravindu Senal Fernando <ravinduf@rocketmail.com>
2021-06-21 06:57:50 +0530

    added state to the filter component

--- src/components/Filter/Filter.js
+++ src/components/Filter/Filter.js
@@ -1,13 +1,21 @@
+import { useState } from 'react';
+
 const Filter = () => {
+  const [region, setRegion] = useState("");
+
+  const handleChange = (e) => {
+    setRegion(e.target.value);
+  }
+
   return (
     <div className="filter">
-      <select>
+      <select onChange={handleChange}>
         <option readOnly hidden defaultValue>Filter by region</option>
-        <option value="">Africa</option>
-        <option value="">America</option>
-        <option value="">Asia</option>
-        <option value="">Europe</option>
-        <option value="">Oceania</option>
+        <option value="Africa">Africa</option>
+        <option value="America">America</option>
+        <option value="Asia">Asia</option>
+        <option value="Europe">Europe</option>
+        <option value="Oceania">Oceania</option>
       </select>
     </div>
   )
