803775201c39818d917af0a610e8ae5d4d650afa
Ravindu Senal Fernando <ravinduf@rocketmail.com>
2021-06-21 08:17:23 +0530

    basic useEffect to build searchTerm

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import { useEffect } from 'react'
+
 import { useSelector } from 'react-redux';
 import classNames from 'classnames';
 
@@ -8,9 +10,21 @@ import Filter from './components/Filter/Filter';
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
 
+let api_url = process.env.REACT_APP_API_URL;
+
 function App() {
 
   const { dark } = useSelector((state) => state.darkTheme);
+  const searchTerm = useSelector((state) => state.searchTerm);
+
+  useEffect(() => {
+    if (searchTerm.isSearchTerm){
+      api_url = `${api_url}/${searchTerm.url}/${searchTerm.searchTerm}`
+    }
+
+   console.log(api_url);
+  }, [searchTerm])
+
 
   return (
     <div className={classNames('theme', dark ? 'theme--dark' : 'theme--default')}>
