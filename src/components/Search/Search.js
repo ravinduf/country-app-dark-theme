b33aa1185e9f1b6249b77512a73a2fe1a3d5c265
Ravindu Senal Fernando <ravinduf@rocketmail.com>
2021-06-23 23:34:14 +0530

    added debounce to update the searchTerm periodically

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -1,5 +1,7 @@
+import { useCallback } from 'react'
 import { ReactComponent as SearchSvg } from '../../assets/images/search.svg';
 import { useDispatch } from 'react-redux'
+import debounce from 'lodash.debounce';
 
 import Form from 'react-bootstrap/Form'
 // import Row from 'react-bootstrap/Row'
@@ -9,8 +11,13 @@ import { setSearchTerm } from '../../actions/searchTerm'
 const Search = () => {
   const dispatch = useDispatch();
   
+  const debouncedHandleChange = useCallback(
+    debounce((value) => dispatch(setSearchTerm("name",value)), 1000)
+  );
+
   const handleChange = (e) => {
-    dispatch(setSearchTerm("name", e.target.value))
+    // dispatch(setSearchTerm("name", e.target.value))
+    debouncedHandleChange(e.target.value);
   }
 
   return (
